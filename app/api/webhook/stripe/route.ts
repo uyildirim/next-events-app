6e0c31a5373fc7cf99de703adf5b9947cb61ae3e
uyildirim <uguryildirim1903@gmail.com>
2024-03-28 20:14:22 +0300

    log

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -3,6 +3,8 @@ import { NextResponse } from "next/server";
 import { createOrder } from "@/lib/actions/order.actions";
 
 export async function POST(request: Request) {
+  console.log("Req", request);
+
   const body = await request.text();
 
   const sig = request.headers.get("stripe-signature") as string;
